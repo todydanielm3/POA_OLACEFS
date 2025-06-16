5bb155f93fb1eaa81c28979c3ff7d1361a34bca2
Daniel Moraes <daniel8moraess@gmail.com>
2025-06-16 14:34:36 -0300

    poaa

--- login.py
+++ login.py
@@ -3,12 +3,14 @@ import streamlit as st
 
 def show_login():
     st.title("🔐 Login - POA OLACEFS")
-
     username = st.text_input("Usuário")
     password = st.text_input("Senha", type="password")
 
+    cred_user = st.secrets["credentials"]["username"]
+    cred_pass = st.secrets["credentials"]["password"]
+
     if st.button("Entrar"):
-        if username == "poa_mvp" and password == "olacefs2025":
+        if username == cred_user and password == cred_pass:
             st.session_state.authenticated = True
             st.rerun()
         else:
